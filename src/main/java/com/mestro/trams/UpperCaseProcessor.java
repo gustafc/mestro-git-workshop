6e372acc3addf7026c42dbd4099b52fdefb33dbf
Stefan <jonsen.stefan@gmail.com>
2016-02-08 14:08:13 +0100

    Fixed TODO

--- src/main/java/com/mestro/trams/UpperCaseProcessor.java
+++ src/main/java/com/mestro/trams/UpperCaseProcessor.java
@@ -3,9 +3,6 @@ package com.mestro.trams;
 public class UpperCaseProcessor implements Processor {
     @Override
     public String process(String source) {
-        // TODO: Use source.toUpperCase instead
-        String output = "";
-        for (char ch: source.toCharArray()) output += Character.toUpperCase(ch);
-        return output;
+        return source.toUpperCase();
     }
 }
