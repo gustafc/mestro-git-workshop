bc5354b0e657e516fc4a4f527c18ac92b4d98309
Magnus Astner <magnus@mestro.com>
2016-02-08 14:13:07 +0100

    fixed

--- src/main/java/com/mestro/trams/LengthProcessor.java
+++ src/main/java/com/mestro/trams/LengthProcessor.java
@@ -3,9 +3,6 @@ package com.mestro.trams;
 public class LengthProcessor implements Processor {
     @Override
     public String process(String source) {
-        // TODO: Use source.length instead
-        int len = 0;
-        for (char ch: source.toCharArray()) ++len;
-        return len + "";
+        return String.valueOf(source.length());
     }
 }
