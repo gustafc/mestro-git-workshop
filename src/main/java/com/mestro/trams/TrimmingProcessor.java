7846521e0572ee83c96d8187768a9c6c91778d12
Sesdun <mattias.brannstrom.mail@gmail.com>
2016-02-08 14:15:00 +0100

    deregexped trimming

--- src/main/java/com/mestro/trams/TrimmingProcessor.java
+++ src/main/java/com/mestro/trams/TrimmingProcessor.java
@@ -3,7 +3,7 @@ package com.mestro.trams;
 public class TrimmingProcessor implements Processor {
     @Override
     public String process(String source) {
-        // TODO: Make this less cryptic with source.trim instead
-        return source.replaceAll("^\\s*|\\s*$", "");
+
+        return source.trim();
     }
 }
